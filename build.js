b116936ba425fb0016f0fd5ca50309de4af41d02
Adrien Navratil <adrien1975@live.fr>
2017-02-18 19:16:19 +0100

    Base template

---
+++ build.js
@@ -0,0 +1,5 @@
+module.exports = function (craftix, config) {
+    // If you want to apply things like Sass compilation, do it here,
+    // but don't forget to change buildFolder to something like 'build' in the config
+    // (or wherever you put your build files), or let '.' if you do not build anything
+};
---
+++ main.js
@@ -0,0 +1,51 @@
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+const config = require('./craftix');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow;
+
+function createWindow()
+{
+    mainWindow = new BrowserWindow({
+        title: config.title,
+        width: config.size.width,
+        height: config.size.height,
+        frame: !config.undecorated,
+        resizable: config.resizable,
+        autoHideMenuBar: true,
+        center: true
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, config.main),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', function()
+    {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function()
+{
+    if (process.platform !== 'darwin')
+    {
+        app.quit()
+    }
+});
+
+app.on('activate', function()
+{
+    if (mainWindow === null)
+    {
+        createWindow()
+    }
+});
